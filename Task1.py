e2bc93c1e754cf650adc7c080dca2f66d4c70526
Sveta <bdyysht@gmail.com>
2024-01-07 15:45:37 +0300

    Добавлена функция типа One_hot

---
+++ Task1.py
@@ -0,0 +1,28 @@
+"""
+В ячейке ниже представлен код генерирующий DataFrame, 
+которая состоит всего из 1 столбца. 
+Ваша задача перевести его в one hot вид. 
+Сможете ли вы это сделать без get_dummies?
+"""
+import pandas as pd
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+df = pd.DataFrame(lst)
+print(df)
+
+data = pd.DataFrame({'whoAmI':lst})
+print(data.head())
+
+def assign_entity(row):
+    if row['whoAmI'] == 'human':
+        return 1
+    elif row['whoAmI']:
+        return 0
+    else:
+        return 'Unknown'
+
+data['Entity'] = data.apply(assign_entity, axis=1)
+print(data.sample(15))
+
